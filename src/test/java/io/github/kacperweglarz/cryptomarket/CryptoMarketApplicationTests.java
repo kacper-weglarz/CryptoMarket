b26436c00e16bd301714273f81efde0e9eb1a543
Kacper <yanniiwnl@gmail.com>
2025-12-06 21:08:55 +0100

    First commit

---
+++ src/test/java/io/github/kacperweglarz/cryptomarket/CryptoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kacperweglarz.cryptomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
