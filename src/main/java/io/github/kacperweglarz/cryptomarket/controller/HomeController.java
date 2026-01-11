84a76b173e66a87efffef1bb20b5317ae1d23a76
Kacper <yanniiwnl@gmail.com>
2026-01-11 19:56:53 +0100

    Saving backend

--- src/main/java/io/github/kacperweglarz/cryptomarket/controller/HomeController.java
+++ src/main/java/io/github/kacperweglarz/cryptomarket/controller/HomeController.java
@@ -1,12 +1,14 @@
 package io.github.kacperweglarz.cryptomarket.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping()
     public String home() {
         return "Start";
     }
